6f03ed0a3118de1936d53c3e4957685bd07a6d83
itscelo <meritomonteiro1@gmail.com>
2026-03-09 13:30:54 -0400

    added required tests

--- classifyTriangle.test.js
+++ classifyTriangle.test.js
@@ -36,5 +36,10 @@ describe('classifyTriangle: Required input conditions', () => {
     test('ECP: Not a Triangle (a+b=c)', () => expect(classifyTriangle(50, 60, 110)).toBe('Not a Triangle'));
     test('ECP: Not a Triangle (a+c=b)', () => expect(classifyTriangle(50, 110, 60)).toBe('Not a Triangle'));
     test('ECP: Not a Triangle (b+c=a)', () => expect(classifyTriangle(110, 50, 60)).toBe('Not a Triangle'));
+
+    // Additional Tests to reach requirement
+    test('BVT: Min valid Equilateral (1, 1, 1)', () => expect(classifyTriangle(1, 1, 1)).toBe('Equilateral'));
+    test('BVT: Max valid Equilateral (200, 200, 200)', () => expect(classifyTriangle(200, 200, 200)).toBe('Equilateral'));
+    test('ECP: Valid Scalene (10, 11, 12)', () => expect(classifyTriangle(10, 11, 12)).toBe('Scalene'));
 });
 
